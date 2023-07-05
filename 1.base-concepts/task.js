609c4391def60cdd32b2068d1f9401bae4d918d5
DeviAnt8981 <serddce@gmail.com>
2023-07-05 13:45:28 +0500

    First task complited

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b**2-4*a*c;
+
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    let x = -b/(2*a);
+    arr.push(x);
+  } else {
+    let x1 = (-b + Math.sqrt(d) )/(2*a);
+    let x2 = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(x1, x2);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthPercent = percent / 100;
+  let bodyCredit = (amount - contribution) / countMonths;
 }
